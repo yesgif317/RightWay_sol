fd3a6debaad822f396f5971ca9ba8048c1064586
Jang <slow0704@naver.com>
2022-04-18 13:35:00 +0900

    0418 1330 장형우 데이터테이블 스크립트 한글화

--- web/resources/js/demo/datatables-demo.js
+++ web/resources/js/demo/datatables-demo.js
@@ -32,3 +32,21 @@ function selectAll(selectAll)  {
     checkbox.checked = selectAll.checked
   })
 }
+
+var table = $('#dataTable').DataTable({
+  "language": {
+    "emptyTable": "데이터가 없어요.",
+    "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+    "info": "현재 _START_ - _END_ / _TOTAL_건",
+    "infoEmpty": "데이터 없음",
+    "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+    "search": "검색: ",
+    "zeroRecords": "일치하는 데이터가 없어요.",
+    "loadingRecords": "로딩중.",
+    "processing":     "잠시만 기다려 주세요.",
+    "paginate": {
+      "next": "다음",
+      "previous": "이전"
+    }
+  },
+});
