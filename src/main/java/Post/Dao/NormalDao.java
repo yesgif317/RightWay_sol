9faf9774ded63b888b5fb0bcbbd50d561f9a8d7a
Jang <slow0704@naver.com>
2022-05-02 10:16:40 +0900

    0502_1000 장형우 normal 게시판 mapper 등

--- src/main/java/Post/Dao/NormalDao.java
+++ src/main/java/Post/Dao/NormalDao.java
@@ -4,7 +4,17 @@ import Post.Dto.NormalVO;
 import java.util.List;
 
 public interface NormalDao {
-    List<NormalVO> selectAll();
+    List<NormalVO> selectPost();
+
+    List<NormalVO> selectPrj_num();
 
     String insertPost(NormalVO postVO);
+
+    //글 수정 Update
+    String updatePost(NormalVO postVO);
+
+    //글 조회 Update
+    NormalVO viewPost(int no);
+
+    int deletePost(int no);
 }
--- src/main/java/Post/Dao/NormalDaoImpl.java
+++ src/main/java/Post/Dao/NormalDaoImpl.java
@@ -15,13 +15,41 @@ public class NormalDaoImpl implements NormalDao {
     private static final String Namespace = "com.mark.mappers.PostMapper";
 
     //전체 게시글
+
+
     @Override
-    public List<NormalVO> selectAll() {
+    public List<NormalVO> selectPost() {
         return sqlSession.selectList(Namespace+".selectPost");
+    }
+    //프로젝트
+    @Override
+    public List<NormalVO> selectPrj_num() {
+        return sqlSession.selectList(Namespace+".selectPrj_num");
     }
     //게시글 작성
     @Override
     public String insertPost(NormalVO postVO) {
         return sqlSession.insert(Namespace+".insertPost",postVO)+"";
     }
+
+    @Override
+    public String updatePost(NormalVO postVO) {
+
+        //Ibatis Update 사용법
+        return sqlSession.update(Namespace+".updatePost", postVO)+"";
+    }
+
+    @Override
+    public NormalVO viewPost(int no) {
+        //Ibatis Update 사용법
+        return sqlSession.selectOne(Namespace+".viewPost",no);
+    }
+
+    public int deletePost(int no) {
+
+        //Ibatis Update 사용법
+        return sqlSession.delete(Namespace+".deletePost",no);
+    }
+
+
 }
